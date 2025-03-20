42d8ec5632b3a3b2d42ab4a63fc3b22c97059701
AOMelville <aomelville@outlook.com>
2025-03-20 13:34:20 -0700

    Abundance.py

--- Abundance.py
+++ Abundance.py
@@ -5,7 +5,7 @@ Created on Thu Feb 20 10:29:36 2025
 @author: User
 """
 
-#Cell abundance from Ecotaxa output Summary->Abundance by NONE
+#Using the output from Ecotaxa Summary->Abundance by NONE, this code will calculate the total counts ('Count') and the cell concentration ('Cells')
 
 import os
 import pandas as pd
@@ -34,11 +34,20 @@ for files in os.listdir(directory):
         df['Taxa']=data['taxonid']
         df['Count']=data['count'].astype(float)
         
-        #Assuming chaing length of 6 (Chaetoceros sp.), 8 (Rhizosolenia), 2 (Thalassiosira)
-        df['Cells']=np.where((df['Taxa']=="Chaetoceros sp."), df['Count']*6,
-                                                 np.where((df['Taxa']=="Rhizosolenia"), df['Count']*8, 
-                                                          np.where((df['Taxa']=="Thalassiosira"), df['Count']*2, 
-                                                                   df['Count']*1)))        
-        totalsampleabund = df.groupby(['Sample', 'Taxa']).sum()
+        #Assuming chaing length
+        df['Cells']=np.where((df['Taxa']=="Asterionellopsis"), df['Count']*1.3, 
+                             np.where((df['Taxa']=="centric diatoms"), df['Count']*1.44,
+                                      np.where((df['Taxa']=="Chaetoceros sp."), df['Count']*5.7, 
+                                               np.where((df['Taxa']=="chain diatom"), df['Count']*5.94,
+                                                        np.where((df['Taxa']=="cylindrical chain forming centric diatoms"), df['Count']*1.62,
+                                                                 np.where((df['Taxa']=="Eucampia zodiacus"), df['Count']*3.64,
+                                                                          np.where((df['Taxa']=="Pseudo-nitzschia"), df['Count']*1.92,
+                                                                                   np.where((df['Taxa']=="Skeletonema"), df['Count']*6.16,
+                                                                                            np.where((df['Taxa']=="Thalassionema"), df['Count']*2.75, 
+                                                                                                     np.where((df['Taxa']=="Thalassiosira"), df['Count']*2.22,
+                                                                                                              np.where((df['Taxa']=="thalassiosira chain"), df['Count']*2.22,
+                                                                                                                       np.where((df['Taxa']=="Thalassiosira rotula"), df['Count']*1.14, 
+                                                                                                                                df['Count']*1))))))))))))    
+        totalsampleabund = df.groupby(['Sample', 'Taxa']).sum()  
         
 totalsampleabund.to_csv('C:/file/path.csv')
