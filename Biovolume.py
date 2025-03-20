16f09bb40f5ac86e6f98a0bd453213a41d669632
AOMelville <aomelville@outlook.com>
2025-03-20 10:24:00 -0700

    Add files via upload

---
+++ Biovolume.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 10:05:39 2025
+
+@author: OM
+"""
+
+#Biovolume calculation from Ecotaxa tsv output "General export separated by NONE"
+#For equations see planktoscope manual and ecotaxa->export->summary->formulae
+
+#Math.sqrt does not work but numpy.sqrt works!
+#** means exponent
+
+import os
+import pandas as pd
+import math
+
+#Set working directory
+directory = os.chdir('C:/Users/User/Desktop/DFO Contract 2025/Biovolume/general_export_test/')
+#Name extension
+ext = ('.tsv')
+
+#Print the file names for the files being processed
+for files in os.listdir(directory):
+    if files.endswith(ext): print(files)
+    
+#Create dataframe
+df = pd.DataFrame()
+
+#Read in tsv file, could be more than one too but then need to add append function!!!
+for files in os.listdir(directory):
+    if files.endswith(ext):
+        data=pd.read_csv(files, sep='\t') 
+        
+        #Input data to dataframe as a number (float) and convert to correct units
+        df['Sample']=data['sample_id']
+        df['Cruise']=data['sample_project']
+        df['Taxa']=data['object_annotation_category']
+        df['Pixelmm']=(data['process_pixel'].astype(float))*0.001  
+        df['Majormm']=(data['object_major'].astype(float))*df['Pixelmm']
+        df['Minormm']=(data['object_minor'].astype(float))*df['Pixelmm']
+        df['Area']=(data['object_area'].astype(float))*df['Pixelmm']*df['Pixelmm']
+        df['ImagedV']=data['acq_imaged_volume'].astype(float)
+        df['SamplevmL']=(data['sample_total_volume'].astype(float))*1000
+        df['ConcV']=data['sample_concentrated_sample_volume'].astype(float)
+        
+        #Choose biovolume equation
+        #Calculate elipsoid volume (Ebv) in mm^3
+        df['BV']=((4/3)*math.pi)*((df['Majormm']*(0.5))*(df['Minormm']*(0.5))*(df['Minormm']*(0.5)))
+        #Calcaulte the individual volume as shown in Ecotaxa
+        #df['BV']=(4/3)*math.pi*(np.sqrt((df['Areamm2'])/math.pi)*df['Sizemm'])**3
+
+        #Calculate mm3/mL in subsample for each taxa
+        df['BV sub sample'] = df['BV']/df['ImagedV']
+        #Use C1V1=C2V2 to determine the real mm3/mL for each taxa
+        df['Biovolume mm3/mL']=(df['BV sub sample']*df['ConcV']/df['SamplevmL'])
+        #Convert mm3/mL to mm3/m3 to get a biovolume per m3 for each taxa
+        df['Biovolume mm3/m3']=df['Biovolume mm3/mL']*1000000
+        
+        #Total biovolume per sample
+        biovolumes=df[["Sample", "BV sub sample", "Biovolume mm3/mL", "Biovolume mm3/m3"]]
+        sampleBV = biovolumes.groupby('Sample').sum()
+        
+        #Total biovolume per taxa for whole project (all files)
+        biovolumet=df[["Taxa", "BV sub sample", "Biovolume mm3/mL", "Biovolume mm3/m3"]]
+        taxaBV = biovolumet.groupby('Taxa').sum()
+        
+        #Total biovolume of each taxa per sample
+        biovolumets=df[["Sample", "Taxa", "BV sub sample", "Biovolume mm3/mL", "Biovolume mm3/m3"]]        
+        taxasampleBV=biovolumets.groupby(['Sample', 'Taxa']).sum()
+        
+        #Total biovolume of each taxa by cruise
+        biovolumec=df[["Cruise", "Taxa", "BV sub sample", "Biovolume mm3/mL", "Biovolume mm3/m3"]]        
+        cruiseBV=biovolumec.groupby(['Cruise', 'Taxa']).sum()
+
+sampleBV.to_csv('C:/Users/User/Desktop/DFO Contract 2025/Biovolume/general_export_test/totalsamplebiovolume.csv')
+taxaBV.to_csv('C:/Users/User/Desktop/DFO Contract 2025/Biovolume/general_export_test/totaltaxabiovolume.csv')
+taxasampleBV.to_csv('C:/Users/User/Desktop/DFO Contract 2025/Biovolume/general_export_test/taxabysamplebiovolume.csv')
+cruiseBV.to_csv('C:/Users/User/Desktop/DFO Contract 2025/Biovolume/general_export_test/cruisetaxabiovolume.csv')
+
+
+#COULD ALSO ADD CALCULATION FOR AVERAGE BIOVOLUME OF TAXA, IF THAT DATA IS INTERESTING
+      
+        
---
+++ Concentration in cells_chain length assumed.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 10 10:06:20 2025
+
+@author: User
+"""
+
+#Concentration calculation from Ecotaxa tsv output "General export separated by NONE"
+
+import os
+import pandas as pd
+import numpy as np
+
+#Set working directory
+directory = os.chdir('C:/Users/User/Desktop/DFO Contract 2025/Concentration/')
+#Name extension
+ext = ('.tsv')
+
+#Print the file names for the files being processed
+for files in os.listdir(directory):
+    if files.endswith(ext): print(files)
+    
+#Create dataframe
+df = pd.DataFrame()
+
+#Read in tsv file, could be more than one too but then need to add append function!!!
+for files in os.listdir(directory):
+    if files.endswith(ext):
+        data=pd.read_csv(files, sep='\t') 
+        
+        #Input data to dataframe as a number (float) and convert to correct units
+        df['Sample']=data['sample_id']
+        df['Cruise']=data['sample_project']
+        df['Taxa']=data['object_annotation_category']
+        #Count number of each taxa in each sample
+        df['Total abundance'] = df.groupby(['Sample','Taxa'])['Taxa'].transform('count')
+        #Input volume data
+        df['ImagedV']=data['acq_imaged_volume'].astype(float)
+        df['SamplevmL']=(data['sample_total_volume'].astype(float))*1000
+        df['ConcV']=data['sample_concentrated_sample_volume'].astype(float)
+        #Drop all the rows with duplicate data, including counts of 0
+        df=df.drop_duplicates()
+        
+        #Assuming chaing length of 6 (Chaetoceros sp.), 8 (Rhizosolenia), 2 (Thalassiosira)
+        df['Cells']=np.where((df['Taxa']=="Asterionellopsis"), df['Total abundance']*1.3, 
+                             np.where((df['Taxa']=="centric diatoms"), df['Total abundance']*1.44,
+                                      np.where((df['Taxa']=="Chaetoceros sp."), df['Total abundance']*5.7, 
+                                               np.where((df['Taxa']=="chain diatom"), df['Total abundance']*5.94,
+                                                        np.where((df['Taxa']=="cylindrical chain forming centric diatoms"), df['Total abundance']*1.62,
+                                                                 np.where((df['Taxa']=="Eucampia zodiacus"), df['Total abundance']*3.64,
+                                                                          np.where((df['Taxa']=="Pseudo-nitzschia"), df['Total abundance']*1.92,
+                                                                                   np.where((df['Taxa']=="Skeletonema"), df['Total abundance']*6.16,
+                                                                                            np.where((df['Taxa']=="Thalassionema"), df['Total abundance']*2.75, 
+                                                                                                     np.where((df['Taxa']=="Thalassiosira"), df['Total abundance']*2.22,
+                                                                                                              np.where((df['Taxa']=="thalassiosira chain"), df['Total abundance']*2.22,
+                                                                                                                       np.where((df['Taxa']=="Thalassiosira rotula"), df['Total abundance']*1.14, 
+                                                                                                                                df['Total abundance']*1))))))))))))
+        #Convert the total cell abundance to concentration
+        df['Concentrated (cells/mL)']=((df['Cells'])/df['ImagedV'])*(df['ConcV'])
+        df['Total concentration (cells/mL)']=df['Concentrated (cells/mL)']/df['SamplevmL']
+        df['Total concentration (cells/m3']=df['Total concentration (cells/mL)']*1000000
+        
+
+df.to_csv('C:/Users/User/Desktop/DFO Contract 2025/Concentration/Concentration per sample in cells.csv')
